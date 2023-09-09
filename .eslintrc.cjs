8e20372fe9663584833f6761fce8755e9289f9b3
riteshgeeky <ritesh.benjwal@geeky.dev>
2023-09-09 06:30:10 +0530

    Project setup with eslint, prettier setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+/* eslint-env node */
+module.exports = {
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        tsConfigRootDir: '__dirname',
+        project: ['./tsconfig.json'],
+    },
+    plugins: ['@typescript-eslint'],
+    root: true,
+    rules: {
+        'no-console': 'error',
+        '@typescript-eslint/no-unused-vars': 'warn',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-empty-function': 'warn',
+        // 'arrow-body-style': ['error', 'always'],
+    },
+};
