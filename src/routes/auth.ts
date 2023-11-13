b03524b00e11a7a1141f2c98415dffe1d7506255
riteshbenjwal <riteshbenjwal7@gmail.com>
2023-11-13 12:25:42 +0530

    persist user test case add

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,18 @@
 import { AuthController } from '../controllers/AuthController';
 import express from 'express';
+import { UserService } from '../services/UserService';
+import { AppDataSource } from '../config/data-source';
+import { User } from '../entity/User';
 
 const router = express.Router();
 
-const authController = new AuthController();
+//Repo
+const userRepository = AppDataSource.getRepository(User);
+
+// Services to Inject
+const userService = new UserService(userRepository);
+
+const authController = new AuthController(userService);
 
 router.post('/register', (req, res) => authController.register(req, res));
 
