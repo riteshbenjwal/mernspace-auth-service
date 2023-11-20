b2666056a545ae66a5ce94c430e85774501059c3
riteshbenjwal <riteshbenjwal7@gmail.com>
2023-11-20 21:56:38 +0530

    token refactoring

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -5,16 +5,20 @@ import { UserService } from '../services/UserService';
 import { AppDataSource } from '../config/data-source';
 import { User } from '../entity/User';
 import registerValidator from '../validators/register-validator';
+import { TokenService } from '../services/TokenService';
+import { RefreshToken } from '../entity/RefreshToken';
 
 const router = express.Router();
 
 //Repo
 const userRepository = AppDataSource.getRepository(User);
+const refreshTokenRepository = AppDataSource.getRepository(RefreshToken);
 
 // Services to Inject
 const userService = new UserService(userRepository);
+const tokenService = new TokenService(refreshTokenRepository);
 
-const authController = new AuthController(userService, logger);
+const authController = new AuthController(userService, logger, tokenService);
 
 router.post(
     '/register',
